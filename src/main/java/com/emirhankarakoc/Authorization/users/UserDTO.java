a58876b9cd435991aa769696d9be7d694bb2c86f
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-04-04 16:41:34 +0300

    406 bug fixed
    https://stackoverflow.com/a/60815160/23739884

--- src/main/java/com/emirhankarakoc/Authorization/users/UserDTO.java
+++ src/main/java/com/emirhankarakoc/Authorization/users/UserDTO.java
@@ -3,8 +3,10 @@ package com.emirhankarakoc.Authorization.users;
 import jakarta.persistence.Entity;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class UserDTO {
     private String id;
     private String username;
